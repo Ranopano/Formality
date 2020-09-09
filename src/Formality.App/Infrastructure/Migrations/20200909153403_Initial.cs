016ecaad1cc0c16775c5388b759a71fee2e2b27b
Ranopano <tehmeru@gmail.com>
2020-09-09 23:19:19 +0300

    Add sqlite migration.

---
+++ src/Formality.App/Infrastructure/Migrations/20200909153403_Initial.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Formality.App.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 2000, nullable: false),
+                    StateId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormFields",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 256, nullable: false),
+                    Label = table.Column<string>(maxLength: 2000, nullable: false),
+                    Placeholder = table.Column<string>(nullable: true),
+                    FormId = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormFields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormFields_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Submissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreateDateTime = table.Column<DateTime>(nullable: false),
+                    FormId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Submissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Submissions_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FieldValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FieldId = table.Column<int>(nullable: false),
+                    Value = table.Column<string>(maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FieldValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FieldValues_FormFields_FieldId",
+                        column: x => x.FieldId,
+                        principalTable: "FormFields",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormFieldRule",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Type = table.Column<int>(nullable: false),
+                    FieldId = table.Column<int>(nullable: false),
+                    Data = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormFieldRule", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormFieldRule_FormFields_FieldId",
+                        column: x => x.FieldId,
+                        principalTable: "FormFields",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubmissionValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FieldId = table.Column<int>(nullable: false),
+                    SubmissionId = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Value = table.Column<string>(maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubmissionValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubmissionValues_FormFields_FieldId",
+                        column: x => x.FieldId,
+                        principalTable: "FormFields",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SubmissionValues_Submissions_SubmissionId",
+                        column: x => x.SubmissionId,
+                        principalTable: "Submissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FieldValues_FieldId",
+                table: "FieldValues",
+                column: "FieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormFieldRule_FieldId",
+                table: "FormFieldRule",
+                column: "FieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormFields_FormId",
+                table: "FormFields",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormFields_Name",
+                table: "FormFields",
+                column: "Name",
+                unique: true,
+                filter: "Deleted != 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Forms_Name",
+                table: "Forms",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Submissions_FormId",
+                table: "Submissions",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubmissionValues_FieldId",
+                table: "SubmissionValues",
+                column: "FieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubmissionValues_SubmissionId",
+                table: "SubmissionValues",
+                column: "SubmissionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FieldValues");
+
+            migrationBuilder.DropTable(
+                name: "FormFieldRule");
+
+            migrationBuilder.DropTable(
+                name: "SubmissionValues");
+
+            migrationBuilder.DropTable(
+                name: "FormFields");
+
+            migrationBuilder.DropTable(
+                name: "Submissions");
+
+            migrationBuilder.DropTable(
+                name: "Forms");
+        }
+    }
+}
